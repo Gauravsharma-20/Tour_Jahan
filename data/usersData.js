c4c2f98aaa52b846ca1e04c0fb658674eceb8df9
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2022-03-23 18:43:05 +0530

    Added users and booking data

---
+++ data/usersData.js
@@ -0,0 +1,59 @@
+export const usersData = [
+    {
+        id:1,
+        AgeCategory:"Adult",
+        Contact:"+91-1245879654",
+        Gender:"Male",
+        Name:"Ram Singh",
+        Nationality:"Local"
+    },
+    {
+        id:2,
+        AgeCategory:"Adult",
+        Contact:"+91-1212879654",
+        Gender:"Male",
+        Name:"Chatur Raj",
+        Nationality:"Local"
+    },
+    {
+        id:3,
+        AgeCategory:"Senior",
+        Contact:"+91-123002654",
+        Gender:"Female",
+        Name:"Aarushi",
+        Nationality:"Local"
+    },
+    {
+        id:4,
+        AgeCategory:"Adult",
+        Contact:"+91-123492654",
+        Gender:"Male",
+        Name:"Palak",
+        Nationality:"Local"
+    },
+    {
+        id:5,
+        AgeCategory:"Adult",
+        Contact:"+91-1239265400",
+        Gender:"Male",
+        Name:"Mikka Shashtri",
+        Nationality:"Local"
+    },
+    {
+        id:6,
+        AgeCategory:"Adult",
+        Contact:"+1-129092654",
+        Gender:"Female",
+        Name:"Jenny",
+        Nationality:"Foreigner"
+    },
+    {
+        id:7,
+        AgeCategory:"Adult",
+        Contact:"+91-123292654",
+        Gender:"Male",
+        Name:"Joseph",
+        Nationality:"Foreigner"
+    },
+
+]
